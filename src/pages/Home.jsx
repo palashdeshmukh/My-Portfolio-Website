9c019d1d5d61a2c717a19aaec3cc2e8a6207a4cc
palashdeshmukh <palash.deshmukh02@gmail.com>
2023-10-27 19:31:21 +0530

    Removed the Footer import from the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../pages-styles/home.css'
 import Navbar from '../component/Navbar';
-import Footer from '../component/Footer';
 function Home () {
   return (
     <>
